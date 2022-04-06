ba27c12e489406cdc9654cfa43659976c5097ef7
dialejo24 <dialejosi530@gmail.com>
2022-04-06 16:50:58 -0500

    function operate created

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,19 @@
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return null;
+    }
+}
+
+
 function add(a, b) {
     return a + b;
 }
